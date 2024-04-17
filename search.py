c6222b7a4e0e08380d25384310550f90767dd5ba
YASH GOSAVI <yashgosavi@YASHs-MacBook-Air.local>
2024-04-17 19:00:44 +0530

    first commit

--- search.py
+++ search.py
@@ -12,18 +12,25 @@ def scrape_flipkart(product_name):
     headers = {
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'
     }
-    response = requests.get(flipkart_url, headers=headers)
-    soup = BeautifulSoup(response.content, 'html.parser')
-    product_cards = soup.find_all("div", class_="_1AtVbE")
-    for card in product_cards:
-        title_element = card.find("div", class_="_4rR01T")
-        if title_element:
-            title = title_element.text.strip()
-            image_url = card.find("img")['src']
-            product_link = "https://www.flipkart.com" + card.find("a")['href']
-            products.append({"title": title, "image_url": image_url, "product_link": product_link})
+    try:
+        response = requests.get(flipkart_url, headers=headers)
+        response.raise_for_status()  # Raise exception for HTTP errors
+        soup = BeautifulSoup(response.content, 'html.parser')
+        product_cards = soup.find_all("div", class_="_1AtVbE")
+        for card in product_cards:
+            title_element = card.find("div", class_="_4rR01T")
+            if title_element:
+                title = title_element.text.strip()
+                image_url = card.find("img")['src']
+                product_link = "https://www.flipkart.com" + card.find("a")['href']
+                products.append({"title": title, "image_url": image_url, "product_link": product_link})
+    except requests.RequestException as e:
+        # Handle request exception
+        print("Error during request:", e)
+    except Exception as e:
+        # Handle other exceptions
+        print("An error occurred:", e)
     return products
-
 
 @app.route('/search', methods=['POST'])
 def search_products():
