a48e0ffdb0ce687fe646794d602aeed756f81f8d
YASH GOSAVI <yashgosavi@YASHs-MacBook-Air.local>
2024-04-17 12:39:27 +0530

    first commit

--- search.py
+++ search.py
@@ -1,8 +1,10 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
+from flask_cors import CORS
 import requests
 from bs4 import BeautifulSoup
 
 app = Flask(__name__)
+CORS(app)
 
 def scrape_flipkart(product_name):
     products = []
@@ -22,12 +24,21 @@ def scrape_flipkart(product_name):
             products.append({"title": title, "image_url": image_url, "product_link": product_link})
     return products
 
+@app.route('/search', methods=['GET', 'POST'])
+def search_products():
+    if request.method == 'GET':
+        product_name = request.args.get('product_name')
+    elif request.method == 'POST':
+        data = request.json
+        product_name = data.get('product_name')
+    else:
+        return jsonify({"error": "Method not allowed"}), 405
+    
+    if not product_name:
+        return jsonify({"error": "Product name not provided"}), 400
 
-@app.route('/search/<product_name>')
-def search_products(product_name):
     products = scrape_flipkart(product_name)
     return jsonify({"products": products})
 
 if __name__ == '__main__':
-        app.run(host='0.0.0.0', port=10000, debug=True)
-
+    app.run(host='0.0.0.0', port=10000, debug=True)
